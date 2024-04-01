cfc722b4c13186cc095bee95559d1e924240ed50
Rabee Zyoud <rabee05@hotmail.com>
2024-04-01 01:16:45 +0000

    created config file

---
+++ config/config.py
@@ -0,0 +1,24 @@
+DATA_DIR = {
+    'input': 'data/input',
+    'output': 'data/output',
+    'vector_stores': 'data/vector_stores'
+}
+
+OLLAMA_SERVER = {
+    'base_url': 'http://192.168.86.153',
+    'port': '11434',
+    'embedding_model': 'llama2',
+    'llm_model': 'mistral'
+
+}
+
+UI = {
+    'css_file': 'css/style.css'
+}
+
+
+SPLITTER_OPS = {
+    'size': 3000,
+    'overlap': 200,
+
+}
